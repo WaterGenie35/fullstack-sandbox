ef623a58052415585ec6e5d148cf6593971b075c
WaterGenie35 <thirdwatergenie@gmail.com>
2024-04-13 06:58:09 +0700

    Add security practices

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Security Practices
+// https://expressjs.com/en/advanced/best-practice-security.html
+app.use(helmet());
+app.disable("x-powered-by");
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+// Deviate from express' default error responses (part of security practice)
+app.use((request, response, next) => {
+  response.status(404).send("Error: Not found");
+});
+
+app.use((error, request, response, next) => {
+  console.error(error.stack);
+  response.status(500).send("Error: Something went wrong");
 });
 
 app.listen(port, () => {
