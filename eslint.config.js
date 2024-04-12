6c0fac486866ef985cda527df12ff8d5d26ab468
WaterGenie35 <thirdwatergenie@gmail.com>
2024-04-13 04:23:52 +0700

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,29 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import js from "@eslint/js";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+import jsdoc from "eslint-plugin-jsdoc";
 
 export default [
-    js.configs.recommended,
-   {
-        languageOptions: {
-                sourceType: 'module',
-                globals: {
-                    ...globals.browser
-                }
-        },
-       rules: {
-           'no-unused-vars': 'warn',
-           'no-undef': 'warn'
-       }
-   }
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser
+      }
+    },
+    plugins: {
+      jsdoc
+    },
+    rules: {
+      // eslint
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+
+      // jsdoc
+      "jsdoc/no-undefined-types": "warn"
+    }
+  }
 ];
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+import Config from 'prettier';
+
+/** @type {Config} */
+const config = {
+    semi: true,
+    trailingComma: 'none'
+};
+
+export default config;
