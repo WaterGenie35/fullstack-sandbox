f35c07a09a9f2fdbd64bc70e116db05d62122262
WaterGenie35 <thirdwatergenie@gmail.com>
2024-04-13 08:55:29 +0700

    Add performance and reliability practices

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
+import compression from "compression";
+import Debug from "debug";
 import express from "express";
 import helmet from "helmet";
 
 const app = express();
 const port = 3000;
+const debug = Debug("fullstack-sandbox");
 
-// Security Practices
+// Security practices
 // https://expressjs.com/en/advanced/best-practice-security.html
 app.use(helmet());
 app.disable("x-powered-by");
+
+// Performance and reliability practices
+// Remove compression middleware when we have it at the reverse proxy level
+app.use(compression());
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
@@ -24,5 +31,6 @@ app.use((error, request, response, next) => {
 });
 
 app.listen(port, () => {
+  debug("Test debug");
   console.log(`Example app listening on port ${port}!`);
 });
