25758c8ec40af264d58d8f3e8c8bc124d2d8446d
WaterGenie35 <thirdwatergenie@gmail.com>
2024-05-20 22:56:50 +0700

    Don't print sensitive info

--- drizzle/postgresql-migrate.js
+++ drizzle/postgresql-migrate.js
@@ -6,7 +6,10 @@ import drizzleConfig from "./postgresql-drizzle.config.mjs";
 import * as schema from '../src/example-sqlite/sqlite-schema.mjs';
 
 console.log(`Migration folder:\t${ drizzleConfig.out }`);
-console.log(`Using postgresql db:\t${ JSON.stringify(drizzleConfig.dbCredentials) }`);
+
+const credentials = { ...drizzleConfig.dbCredentials };
+delete credentials.password;
+console.log(`Using postgresql db:\t${ JSON.stringify(credentials) }`);
 
 const postgresqlClient = postgres(drizzleConfig.dbCredentials);
 const drizzleDB = drizzle(postgresqlClient, { schema: schema });
